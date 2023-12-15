1be340aadf162f2090e652e80b874c7300723f83
AnatoliyPolubabin <117819335+AnatoliyPolubabin@users.noreply.github.com>
2023-12-15 13:39:39 +0300

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-// utils/constants.js
+// utils/httpStatusCodes.js
 const HTTP_STATUS_CODES = {
   OK: 200,
   CREATED: 201,
@@ -7,4 +7,4 @@ const HTTP_STATUS_CODES = {
   INTERNAL_SERVER_ERROR: 500,
 };
 
-module.exports = { HTTP_STATUS_CODES };
+module.exports = HTTP_STATUS_CODES;
