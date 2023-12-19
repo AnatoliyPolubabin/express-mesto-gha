22ad9cf6d9599d889217183443a881984ab7377f
AnatoliyPolubabin <117819335+AnatoliyPolubabin@users.noreply.github.com>
2023-12-19 20:37:39 +0300

    14pr

--- middlewares/extractJwt.js
+++ middlewares/extractJwt.js
@@ -1,7 +1,14 @@
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
-  if (token) {
-    req.headers.authorization = `Bearer ${token}`;
+  if (!token) {
+    throw new AuthenticatorAssertionResponse('Авторизуйтесь');
   }
+  let payload;
+  try {
+    payload = jwt.verity(token, secretOrPublicKey: 'super-strong-secret');
+  } catch (err) {
+    return next(new AuthenticatorAssertionResponse('Авторизуйтесь'));
+  }
+  req.user = payload;
   next();
 };
