4f6de1e0f7dfe9e3d65055dc2e5a59aa4f2059ae
AnatoliyPolubabin <117819335+AnatoliyPolubabin@users.noreply.github.com>
2023-12-21 01:01:25 +0300

    Update extractJwt.js

--- middlewares/extractJwt.js
+++ middlewares/extractJwt.js
@@ -1,20 +1,7 @@
-const jwt = require('jsonwebtoken'); // Убедитесь, что вы импортируете библиотеку jwt
-
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
-  if (!token) {
-    // Если токен отсутствует, бросаем ошибку авторизации
-    return next(new AuthError('Необходима авторизация'));
+  if (token) {
+    req.headers.authorization = `Bearer ${token}`;
   }
-  let payload;
-  try {
-    // Верификация токена
-    payload = jwt.verify(token, 'd285e3dceed844f902650f40'); // Замените 'ваш_секретный_ключ' на ваш секретный ключ
-  } catch (err) {
-    // Если токен не валиден, бросаем ошибку авторизации
-    return next(new AuthError('Необходима авторизация'));
-  }
-  // Если верификация прошла успешно, добавляем payload в объект запроса
-  req.user = payload;
   next();
 };
