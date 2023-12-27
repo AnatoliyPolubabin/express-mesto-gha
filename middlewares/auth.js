1e53b45b5b9249f2cbd137a5bd7ea092c9c9a845
AnatoliyPolubabin <117819335+AnatoliyPolubabin@users.noreply.github.com>
2023-12-27 17:25:10 +0300

    !

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,17 +2,19 @@ const jwt = require('jsonwebtoken');
 const AuthError = require('../errs/AuthError');
 
 // eslint-disable-next-line consistent-return
-module.exports = (reg, res, next) => {
-  const token = req.cookies.jwt;
-  if (!token) {
-    throw new AuthError('Авторизируетесь');
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new AuthError('Авторизируетесь'));
   }
-  Let payload;
+  const token = authorization.replace('Bearer ', '');
+  let payload;
   try {
-    payload = jwt.verify(token,
+    payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
     return next(new AuthError('Авторизируетесь'));
   }
   req.user = payload;
-  next;
-  ｝;
+  next();
+};
